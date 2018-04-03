8f71ea31ad28830db02d0cab2831e4623d3ae3b8
Aliyar F <aliyarfatemi@gmail.com>
2018-04-03 01:50:10 +0000

    Stripe Final Version

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -32,7 +32,7 @@ def create
    )
  
    flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
-   redirect_to user_path(current_user) # or wherever
+   redirect_to wikis_path
  
    # Stripe will send back CardErrors, with friendly messages
    # when something goes wrong.
